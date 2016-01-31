65af519389bdb4665d0a3d8ffe79731e56fec8dd
naumanC <nscgeneral@gmail.com>
2016-01-31 15:22:28 -0500

    Update run_analysis.R
    updated comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,3 @@
-#if(!file.exists("./data")){dir.create("./data")}
-#fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-#download.file(fileUrl,destfile="./data/Dataset.zip")
-#unzip(zipfile="./data/Dataset.zip",exdir="./data")
-#path_rf <- file.path("./data" , "UCI HAR Dataset")
-#files<-list.files(path_rf, recursive=TRUE)
-#files
-
-#dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
-#dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
-
-#dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
-#dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
-
-#dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
-#dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
-
-#str(dataActivityTest)
-
-################
 require(plyr)
 
 # Directories and files
